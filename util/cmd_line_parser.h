7e229a67221e5e0c4e75ae11fd5bf7244bdc983a
Ryan Lustig <lustig@braincorporation.com>
2019-11-07 11:21:54 -0800

    add gitignore

---
+++ util/cmd_line_parser.h
@@ -0,0 +1,13 @@
+//
+//  cmd_line_parser.h
+//  DBoW2
+//
+//  Created by Ryan Lustig on 10/22/19.
+//  Copyright © 2019 Ryan Lustig. All rights reserved.
+//
+
+#ifndef cmd_line_parser_h
+#define cmd_line_parser_h
+
+
+#endif /* cmd_line_parser_h */
---
+++ util/dir_reader.h
@@ -0,0 +1,13 @@
+//
+//  dir_reader.h
+//  DBoW2
+//
+//  Created by Ryan Lustig on 10/22/19.
+//  Copyright © 2019 Ryan Lustig. All rights reserved.
+//
+
+#ifndef dir_reader_h
+#define dir_reader_h
+
+
+#endif /* dir_reader_h */
